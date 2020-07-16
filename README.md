723a8b2d8e7d132829913a6b2d100366703c947f
Lina Sachuk <sachuklina@gmail.com>
2020-07-16 16:16:46 -0600

    add css and js files

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 My first TensorFlow.js project.
 This library allows you to harness huge power of neural nets models that are already build using TensorFlow or Keras, making your JS application look almost like magic. You are also able to create models from scratch with new JavaScript API and train them using client’s GPU processing power.
+
+https://github.com/tensorflow/tfjs-models/tree/master/mobilenet
---
+++ main.js
@@ -0,0 +1,54 @@
+
+// Notice there is no 'import' statement. 'mobilenet' and 'tf' is
+// available on the index-page because of the script tag above.
+
+const img = document.getElementById('img');
+const button = document.getElementById('submit_button');
+const input = document.getElementById('image_url');
+const result = document.getElementById('prediction');
+
+
+
+let model;
+
+button.onclick = () => {
+    const url = input.value;
+    img.src = url;
+    result.innerText = "Loading..."
+}
+
+img.onload = () => {
+    doPrefiction();
+}
+
+
+
+function doPrefiction() {
+    if (model) {
+
+        // Classify the image.
+        model.classify(img).then(predictions => {
+            console.log('Predictions: ');
+            console.log(predictions);
+            showPrediction(predictions);
+        });
+
+    } else {
+        // Load the model.
+        mobilenet.load().then(_model => {
+            model = _model;
+            model.classify(img).then(predictions => {
+                console.log('Predictions: ');
+                console.log(predictions);
+                showPrediction(predictions);
+
+            });
+        });
+    }
+}
+
+function showPrediction(predictions) {
+    result.innerText = "This might be a " + predictions[0].className;
+
+}
+
