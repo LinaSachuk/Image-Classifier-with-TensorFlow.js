e8d4ab28d7e6afa27393c4821c80d4bc181ae103
Lina Sachuk <sachuklina@gmail.com>
2020-07-18 02:14:28 -0600

    add the game logic

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let net;
 
+
+
 const webcamElement = document.getElementById('webcam');
 const classifier = knnClassifier.create();
 
@@ -32,32 +34,121 @@ async function app() {
     };
 
     // When clicking a button, add an example for that class.
-    document.getElementById('class-a').addEventListener('click', () => addExample(0));
-    document.getElementById('class-b').addEventListener('click', () => addExample(1));
-    document.getElementById('class-c').addEventListener('click', () => addExample(2));
+    document.getElementById('class-rock').addEventListener('click', () => addExample(0));
+    document.getElementById('class-paper').addEventListener('click', () => addExample(1));
+    document.getElementById('class-scissors').addEventListener('click', () => addExample(2));
+    document.getElementById('train').addEventListener('click', () => train == true);
 
+
+
+
+
+    // console.log('training');
     while (true) {
         if (classifier.getNumClasses() > 0) {
             const img = await webcam.capture();
-
+            play == false;
             // Get the activation from mobilenet from the webcam.
             const activation = net.infer(img, 'conv_preds');
             // Get the most likely class and confidence from the classifier module.
             const result = await classifier.predictClass(activation);
+            // console.log(result)
 
-            const classes = ['A', 'B', 'C'];
-            document.getElementById('console').innerText = `
-          prediction: ${classes[result.label]}\n
-          probability: ${result.confidences[result.label]}
-        `;
+            const classes = ['rock', 'paper', 'scissors'];
+            document.getElementById('label').innerText = `
+              ${classes[result.label]}
+            `;
+
+
+
+            document.getElementById('confidences').innerText = `
+              probability: ${result.confidences[result.label]}
+            `;
+
+            document.getElementById('play').addEventListener('click',
+                function () {
+
+                    console.log('game!');
+                    var userChoice = document.getElementById('label').textContent;
+                    console.log(userChoice);
+
+                    if (!userChoice) {
+                        // User choice was undefined
+                        document.getElementById('player_choice').innerText = `Player 1, you cheated! Refresh this screen and fight like a man.`;
+
+                    } else {
+                        // Display user choice
+                        document.getElementById('player_choice').innerText = `Player 1: ${userChoice}`;
+
+                    }
+
+                    // Computer choice
+                    var computerChoice = Math.random();
+                    if (computerChoice < 0.34) {
+                        computerChoice = "rock";
+                    } else if (computerChoice <= 0.67) {
+                        computerChoice = "paper";
+                    } else {
+                        computerChoice = "scissors";
+                    }
+
+                    // Display computer choice
+                    document.getElementById('computer_choice').innerText = `
+                    Computer:  ${computerChoice}
+                     `;
+
+                    // var computerChoice = document.getElementById('computer_choice').textContent;
+                    console.log(computerChoice);
+
+
+                    function compare(choice1, choice2) {
+                        console.log('inside!');
+                        console.log('==================');
+                        console.log(choice1, choice2);
+                        console.log(choice1 == choice2);
+
+                        console.log(typeof 'choice1');
+                        console.log(typeof 'choice2');
+                        var n = true;
+                        console.log('==================');
+
+                        return n;
+                    };
+
+
+                    // Run the compare function
+                    var a = userChoice.trim();
+                    var b = computerChoice.trim();
+                    var result = compare(a, b);
+
+
+
+
+
+
+                    // Display results
+                    console.log(result)
+                    // document.write("<p>Result:" + result + "</p>");
+
+                });
 
             // Dispose the tensor to release the memory.
             img.dispose();
         }
 
         await tf.nextFrame();
+
+
+
+
+
+
+
     }
 }
+
+
+
 
 
 app();
