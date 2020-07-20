7e7cd40ab91a8c42f59622644acd5497b1a6327d
Lina Sachuk <sachuklina@gmail.com>
2020-07-20 00:58:47 -0600

    add MobileNet part

--- main.js
+++ main.js
@@ -1,54 +1,54 @@
 
-// // Notice there is no 'import' statement. 'mobilenet' and 'tf' is
-// // available on the index-page because of the script tag above.
+// Notice there is no 'import' statement. 'mobilenet' and 'tf' is
+// available on the index-page because of the script tag above.
 
-// const img = document.getElementById('img');
-// const button = document.getElementById('submit_button');
-// const input = document.getElementById('image_url');
-// const result = document.getElementById('prediction');
-
-
-
-// let model;
-
-// button.onclick = () => {
-//     const url = input.value;
-//     img.src = url;
-//     result.innerText = "Loading..."
-// }
-
-// img.onload = () => {
-//     doPrefiction();
-// }
+const img = document.getElementById('img');
+const button = document.getElementById('submit_button');
+const input = document.getElementById('image_url');
+const result = document.getElementById('prediction');
 
 
 
-// function doPrefiction() {
-//     if (model) {
+let model;
 
-//         // Classify the image.
-//         model.classify(img).then(predictions => {
-//             console.log('Predictions: ');
-//             console.log(predictions);
-//             showPrediction(predictions);
-//         });
+button.onclick = () => {
+    const url = input.value;
+    img.src = url;
+    result.innerText = "Loading..."
+}
 
-//     } else {
-//         // Load the model.
-//         mobilenet.load().then(_model => {
-//             model = _model;
-//             model.classify(img).then(predictions => {
-//                 console.log('Predictions: ');
-//                 console.log(predictions);
-//                 showPrediction(predictions);
+img.onload = () => {
+    doPrefiction();
+}
 
-//             });
-//         });
-//     }
-// }
 
-// function showPrediction(predictions) {
-//     result.innerText = "This might be a " + predictions[0].className;
 
-// }
+function doPrefiction() {
+    if (model) {
+
+        // Classify the image.
+        model.classify(img).then(predictions => {
+            console.log('Predictions: ');
+            console.log(predictions);
+            showPrediction(predictions);
+        });
+
+    } else {
+        // Load the model.
+        mobilenet.load().then(_model => {
+            model = _model;
+            model.classify(img).then(predictions => {
+                console.log('Predictions: ');
+                console.log(predictions);
+                showPrediction(predictions);
+
+            });
+        });
+    }
+}
+
+function showPrediction(predictions) {
+    result.innerText = "This might be a " + predictions[0].className;
+
+}
 
