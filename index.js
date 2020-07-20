18c34740d0142e869a1fdec7183bb5660a75178e
Lina Sachuk <sachuklina@gmail.com>
2020-07-20 02:15:51 -0600

    add transfer learning part

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 let net;
+new ClipboardJS('.btn');
+var clipboard = new ClipboardJS('.btn');
 
+clipboard.on('success', function (e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+
+    e.clearSelection();
+});
+
+clipboard.on('error', function (e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+});
 
 
 const webcamElement = document.getElementById('webcam');
